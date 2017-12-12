b1f5ad39ad9573379a961348c0b84eccf696e62d
Constantin MIRON <cmiron@e1p47.chisinau.42.fr>
2017-12-12 18:01:57 +0200

    gg

--- http/MyWebSite/D03/raw_text.php
+++
@@ -1,4 +0,0 @@
-<?php
-	header('Content-Type: plain/text; charset= UTF-8');
-?>
-<html><body>Hello</body></html>
--- http/MyWebSite/D03/read_img.php
+++
@@ -1,4 +0,0 @@
-<?php
-	header('Content-Type: image/png; charset= UTF-8');
-	readfile("../img/42.png");
-?>
